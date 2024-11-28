0b349d39cfd49e7bcb280853ab09ba2850b3f90e
RaelMartinss <raelpgm2@gmail.com>
2024-11-28 20:36:44 -0300

    style: :lipstick:

--- lib/auth.ts
+++ lib/auth.ts
@@ -23,36 +23,36 @@ export async function encrypt(payload: any) {
   return token;
 }
 
-// export async function decrypt(token: string): Promise<any> {
-//   const secret = await getJwtSecretKey();
-//   const { payload } = await jwtVerify(token, secret);
-//   return payload;
-// }
+export async function decrypt(token: string): Promise<any> {
+  const secret = await getJwtSecretKey();
+  const { payload } = await jwtVerify(token, secret);
+  return payload;
+}
 
-// export async function getUserFromToken(request: NextRequest) {
-//   const token = request.cookies.get('token')?.value;
-//   if (!token) {
-//     console.warn('Nenhum token encontrado no cookie.');
-//     return null;
-//   }
+export async function getUserFromToken(request: NextRequest) {
+  const token = request.cookies.get('token')?.value;
+  if (!token) {
+    console.warn('Nenhum token encontrado no cookie.');
+    return null;
+  }
 
-//   try {
-//     const payload = await decrypt(token);
-//     const user = await db.user.findUnique({
-//       where: { id: payload.id },
-//       select: { id: true, email: true, name: true },
-//     });
+  try {
+    const payload = await decrypt(token);
+    const user = await db.user.findUnique({
+      where: { id: payload.id },
+      select: { id: true, email: true, name: true },
+    });
 
-//     if (!user) {
-//       console.warn(`Usuário com ID ${payload.id} não encontrado no banco de dados.`);
-//     }
+    if (!user) {
+      console.warn(`Usuário com ID ${payload.id} não encontrado no banco de dados.`);
+    }
 
-//     return user;
-//   } catch (error) {
-//     console.error('Erro ao autenticar o usuário a partir do token:', error);
-//     return null;
-//   }
-// }
+    return user;
+  } catch (error) {
+    console.error('Erro ao autenticar o usuário a partir do token:', error);
+    return null;
+  }
+}
 
 export async function setUserCookie(response: NextResponse, token: string) {
   response.cookies.set('token', token, {
